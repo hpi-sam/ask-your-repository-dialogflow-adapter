9de5a722c8ee4d370c848fef8fa42f025f831c66
arne-z <arne.zerndt@student.hpi.de>
2018-12-03 15:40:21 +0100

    Add Date and Author to querry. Add Image Carousels. Ref #6

--- src/app.js
+++ src/app.js
@@ -2,37 +2,21 @@
 import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
 import bodyParser from 'body-parser';
-import { dialogflow, Image } from 'actions-on-google';
+import { dialogflow } from 'actions-on-google';
 import ArtifactController from './controller/ArtifactController';
 import logger from './logger';
 
 const app = express();
 app.use(bodyParser.json());
 
+const dialog = dialogflow();
+
+dialog.intent('Get Artifacts', ArtifactController.getArtifacts);
+
+// Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is tobito.\n There isn\'t any content on this website.');
   logger.info('received GET /');
-});
-
-const dialog = dialogflow();
-
-
-// function esraRequestGetLatest(conv, params) {
-//   // TODO: Request latest image from esra once an esra route is implemented for this.
-// }
-
-// Register handlers for Dialogflow intents
-
-dialog.intent('Get Artifacts', ArtifactController.getArtifacts);
-// dialog.intent('Get latest Artifact', esraRequestGetLatest);
-
-dialog.intent(['Get latest Artifact'], (conv) => {
-  conv.ask('The server can\'t do this yet but have a Cat instead!');
-  conv.ask(new Image({
-    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-    alt: 'A cat',
-  }));
-  logger.info('Get latest Artifact - Responded with cat image.');
 });
 app.post('/', dialog);
 export default app;
--- src/controller/ArtifactController.js
+++ src/controller/ArtifactController.js
@@ -1,60 +1,96 @@
+// @flow
 import axios from 'axios';
 import {
-  Image, BrowseCarousel, BrowseCarouselItem,
+  Image, BrowseCarousel, BrowseCarouselItem, Conversation,
 } from 'actions-on-google';
 import logger from '../logger';
 
+const THRESHHOLD: number = 0;
+const url = 'https://api.askir.me/images';
+
+function setParams(params) {
+  logger.info(JSON.stringify(params));
+  const requestString = {
+    params: {},
+  };
+  if (params.Tag) { requestString.params.search = params.Tag; }
+  if (params.DatePeriod) { requestString.params.date = params.DatePeriod; }
+  if (params.Author) { requestString.params.author = params.Author; }
+  return requestString;
+}
+
+function getGoodImages(response) {
+  const { images } = response.data;
+  const goodImages = [];
+  images.forEach((element) => {
+    if (element.score > THRESHHOLD) { goodImages.push(element); }
+  });
+  return goodImages;
+}
+
+function getHumanReadableTags(goodImages, i) {
+  return goodImages[i].tags ? goodImages[i].tags.join(', ') : 'No Tags';
+}
+
+function makeCarousel(goodImages, params) {
+  const items = [];
+  for (let i = 0; i < goodImages.length && i < 5; i += 1) {
+    items.push(new BrowseCarouselItem({
+      title: `Image ${i + 1}`,
+      url: goodImages[i].url,
+      description: getHumanReadableTags(goodImages, i),
+      image: new Image({
+        url: goodImages[i].url,
+        alt: `${params.Artifact} with the tags: ${getHumanReadableTags(goodImages, i)}`,
+      }),
+    }));
+  }
+  const car = new BrowseCarousel({
+    items,
+  });
+  return car;
+}
+
+function respondMultipleImages(conv, goodImages, params) {
+  conv.ask('Here are the best images we found for your request:');
+  const carousel = makeCarousel(goodImages, params);
+  conv.ask(carousel);
+  logger.info(`Get Artifact - Responded with this carousel ${JSON.stringify(carousel)}`);
+}
+
+function respondOneImage(conv, goodImages, params) {
+  conv.ask('Here is the best image we found for your request:');
+  const image = new Image({
+    url: goodImages[0].url,
+    alt: `${params.Artifact} with the tags: ${getHumanReadableTags(goodImages, 0)}`,
+  });
+  conv.ask(image);
+  logger.info(`Get Artifact - Responded with this image:\n${JSON.stringify(image)}`);
+}
+
+function respondServerError(conv) {
+  logger.info('Error');
+  conv.ask('The server can\'t handle your request right now but have a Cat instead!');
+  conv.ask(new Image({
+    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+    alt: 'A cat',
+  }));
+}
+
 export default {
-  async getArtifacts(conv, params) {
+  async getArtifacts(conv: Conversation, params: Object) {
     try {
-      logger.info(JSON.stringify(params));
-      const url = 'https://api.askir.me/images';
-      const response = await axios.get(url, {
-        params: {
-          searchTerm: params.Tag,
-          // type: params.Artifact,
-          // date: params.DatePeriod,
-        },
-      });
-      const { images } = response.data;
-      if (images.length > 1) {
-        conv.ask('Here are the best images we found for your request:');
-        const items = [];
-        for (let i = 0; i < images.length && i < 5; i += 1) {
-          items.push(new BrowseCarouselItem({
-            title: `Image ${i}`,
-            url: images[i].url,
-            description: images[i].tags ? images[i].tags.join(', ') : 'No Tags',
-            image: new Image({
-              url: images[i].url,
-              alt: `${params.Artifact} with the tags: ${images[i].tags ? images[i].tags.join(', ') : 'No Tags'}`,
-            }),
-          }));
-        }
-        const car = new BrowseCarousel({
-          items,
-        });
-        conv.ask(car);
-        logger.info(`Get Artifact - Responded with this carousel ${JSON.stringify(car)}`);
-      } else if (images.length === 1) {
-        conv.ask('Here is the best image we found for your request:');
-        const image = new Image({
-          url: images[0].url,
-          alt: params.Artifact,
-        });
-        conv.ask(image);
-        logger.info(`Get Artifact - Responded with this image:\n${JSON.stringify(image)}`);
+      const response = await axios.get(url, setParams(params));
+      const goodImages = getGoodImages(response);
+      if (goodImages.length > 1) {
+        respondMultipleImages(conv, goodImages, params);
+      } else if (goodImages.length === 1) {
+        respondOneImage(conv, goodImages, params);
       } else {
         conv.ask('No image matched your serach criteria. We are sorry.');
       }
-      // conv.contexts.set('images', 5, images); // This doesn't work yet.
     } catch (e) {
-      logger.info('Error');
-      conv.ask('The server isn\'t up right now but have a Cat instead!');
-      conv.ask(new Image({
-        url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-        alt: 'A cat',
-      }));
+      respondServerError(conv);
     }
   },
 };
