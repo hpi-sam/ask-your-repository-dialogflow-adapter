ffd13b5b66d7af55d0af3534884e2a9d1a9e0cb9
arne-z <arne.zerndt@student.hpi.de>
2018-12-06 19:28:48 +0100

    Change Tags from array to csv string.

--- src/controller/RequestController.js
+++ src/controller/RequestController.js
@@ -10,7 +10,7 @@ function setParams(params: ConvParams) {
   const requestString = {
     params: {},
   };
-  if (params.Tag) { requestString.params.search = params.Tag; }
+  if (params.Tag) { requestString.params.search = params.Tag.join(', '); }
   if (params.DatePeriod) { requestString.params.start_date = params.DatePeriod.startDate; }
   if (params.DatePeriod) { requestString.params.end_date = params.DatePeriod.endDate; }
   if (params.Author) { requestString.params.author = params.Author; }
