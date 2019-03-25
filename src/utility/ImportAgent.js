de0f9400deff15874389b49a5ed5a96739552342
arne-z <arne.zerndt@student.hpi.de>
2019-03-25 18:15:38 +0100

    Refactored scripts and fixed import overwriting dynamic entities.

--- src/utility/ImportAgent.js
+++ src/utility/ImportAgent.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
 const Dialogflow = require('dialogflow');
 const AdmZip = require('adm-zip');
+const dynamic = require('./dynamicallyGeneratedEntries');
 
+const projectId = process.env.DIALOGFLOW_PROJECT_ID;
+const file = process.argv[2] || './Agent';
 
-async function importAgent(projectId, file = './Agent') {
-  const agentClient = new Dialogflow.v2.AgentsClient();
+async function importAgent(projectId, file) {
+  const agentClient = new Dialogflow.v2.AgentsClient({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  });
   const zip = new AdmZip();
   zip.addLocalFolder(file);
+  dynamic.forEach(entry => zip.deleteFile(entry));
   const buffer = zip.toBuffer();
   await agentClient.importAgent({ parent: projectId, agentContent: buffer });
 }
 
-if (process.argv[3]) {
-  importAgent(process.argv[2], process.argv[3]);
-} else {
-  importAgent(process.argv[2]);
-}
+importAgent(projectId, file);
