fc33896f4193e7e98397c8b040e1a7a513a4f9c8
arne-z <arne.zerndt@student.hpi.de>
2019-04-01 13:47:08 +0200

    Fixes.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,15 @@ import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
 import bodyParser from 'body-parser';
 import { dialogflow, SignIn } from 'actions-on-google';
-import { getArtifacts, startSignIn, getSignIn} from './controller/DialogflowController';
-
-'./controller/DialogflowController';
+import { getArtifacts, startSignIn, getSignIn } from './controller/DialogflowController';
 import logger from './logger';
+
 
 const app = express();
 app.use(bodyParser.json());
 
-const dialog = dialogflow({ clientId: '24456970850-6t9vipqs6smjt3jmfk7db03cr8r1gqsu.apps.googleusercontent.com'});
-dialog.intent('Default Welcome Intent', conv => {
+const dialog = dialogflow({ clientId: '24456970850-6t9vipqs6smjt3jmfk7db03cr8r1gqsu.apps.googleusercontent.com' });
+dialog.intent('Default Welcome Intent', (conv) => {
   conv.ask(new SignIn('To get your account details'));
 });
 dialog.intent('Get Artifacts', getArtifacts);
