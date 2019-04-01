1e36d3b8749511a07999688ef5813b5c50d3f3a8
Adrian Steppat <adrian.steppat@student.hpi.de>
2019-04-01 12:46:20 +0200

    Implement google authentication

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@
 import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
 import bodyParser from 'body-parser';
-import { dialogflow } from 'actions-on-google';
-import getArtifacts from './controller/DialogflowController';
+import { dialogflow, SignIn } from 'actions-on-google';
+import { getArtifacts, startSignIn, getSignIn} from './controller/DialogflowController';
+
+'./controller/DialogflowController';
 import logger from './logger';
 
 const app = express();
 app.use(bodyParser.json());
 
-const dialog = dialogflow();
-
+const dialog = dialogflow({ clientId: '24456970850-6t9vipqs6smjt3jmfk7db03cr8r1gqsu.apps.googleusercontent.com'});
+dialog.intent('Default Welcome Intent', conv => {
+  conv.ask(new SignIn('To get your account details'));
+});
 dialog.intent('Get Artifacts', getArtifacts);
-
+dialog.intent('Sign in', getSignIn);
 // Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is tobito. There isn\'t any content on this website.');
