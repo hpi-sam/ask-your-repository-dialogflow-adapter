12827a6c43a3c4cd6e5291ad6cd6e2ea4dec1de1
arne-z <38384819+arne-z@users.noreply.github.com>
2019-07-15 17:09:42 +0200

    Revert "How did I miss this."
    This reverts commit 539b149fc07b0b6c85a7e61d7c0f6271f33208a4.

--- spec/SampleRequests.js
+++ spec/SampleRequests.js
@@ -391,7 +391,7 @@ export const GetArtifactNoTeamSelectedRequest = {
       },
     }
   ],
-    intent: { name: 'projects/newagent-bdb60/agent/intents/78328cbe-bd4f-492f-ad59-3606cac17310', displayName: 'GetArtifactsNoTeamSelected' },
+    intent: { name: 'projects/newagent-bdb60/agent/intents/78328cbe-bd4f-492f-ad59-3606cac17310', displayName: 'GetArtifacts No Team Selected' },
     intentDetectionConfidence: 1,
     languageCode: 'en',
   },
